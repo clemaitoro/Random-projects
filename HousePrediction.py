c3ec670e16001e38db3201674bfccda3c589a6b7
Andrei Lupica <64814779+clemaitoro@users.noreply.github.com>
2023-02-04 09:53:20 +0100

    Add files via upload

---
+++ HousePrediction.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("kc_house_data.csv")
+
+x = dataset.iloc[:, 3:-1]
+y = dataset.iloc[:, 2]
+
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+
+
+regressor = PolynomialFeatures(degree=4)
+
+x_poly = regressor.fit_transform(x)
+
+x_test,x_train,y_test, y_train = train_test_split(x_poly, y, test_size=0.2, random_state=0)
+
+poly_reg = LinearRegression()
+
+poly_reg.fit(x_train, y_train)
+
+y_pred = poly_reg.predict(x_test)
+
+print([[y_pred]])
+print(y_test)
+
+

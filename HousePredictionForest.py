7fb84b56b419bd1b398d426a93b800da9cb7083a
Andrei Lupica <64814779+clemaitoro@users.noreply.github.com>
2023-02-08 20:58:36 +0100

    House pred with Forest Regression

---
+++ HousePredictionForest.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("kc_house_data.csv")
+
+x = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, 2].values
+
+# y = y.reshape(len(y), 1)
+
+
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+
+
+
+x_test, x_train, y_test, y_train = train_test_split(x, y, random_state=0, test_size=0.2)
+
+regressor = RandomForestRegressor(n_estimators=100)
+
+regressor.fit(x_train, y_train)
+
+y_pred = regressor.predict(x_test).reshape(-1,1)
+
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+

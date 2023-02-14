068ddea0427c165e9cb49b0e42243bc8cc3233a4
Andrei Lupica <64814779+clemaitoro@users.noreply.github.com>
2023-02-14 21:47:12 +0100

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -4,9 +4,8 @@ import numpy as np
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
 
 dataset_train = pd.read_csv("train.csv")
 dataset_test = pd.read_csv("test.csv")
@@ -14,9 +13,9 @@ dataset_test[["Survived"]] = 0
 
 
 
-dataset_train_reorder = dataset_train[["PassengerId", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Name","Ticket", "Cabin", "Embarked", "Survived"]]
+dataset_train_reorder = dataset_train[["PassengerId", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Name","Embarked","Ticket", "Cabin","Survived"]]
 
-dataset_test_reorder = dataset_test[["PassengerId", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare",  "Name", "Ticket", "Cabin", "Embarked", "Survived"]]
+dataset_test_reorder = dataset_test[["PassengerId", "Pclass", "Sex",  "Age", "SibSp", "Parch", "Fare",  "Name","Embarked", "Ticket", "Cabin","Survived"]]
 
 
 
@@ -36,21 +35,20 @@ imputer.fit(x[:, :8])
 x[:, :8] = imputer.transform(x[:, :8])
 
 
-x_test = dataset_test_reorder.iloc[:, :-1].values
+x_test = dataset_test_reorder.iloc[:, :7].values
 y_test = dataset_test_reorder.iloc[:, -1].values
 
-print(y_test)
 
 
 ct = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [2])], remainder="passthrough")
 x_test = np.array(ct.fit_transform(x_test))
 
 imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
-imputer.fit(x[:, :8])
+imputer.fit(x_test[:, :8])
 x_test[:, :8] = imputer.transform(x_test[:, :8])
 
 
-regressor = LinearRegression()
+regressor = SVC(random_state=0, kernel="linear")
 
 regressor.fit(x[:, :8], y)
 
@@ -58,9 +56,17 @@ y_pred = regressor.predict(x_test[:, :8])
 y_pred = y_pred.round(decimals=0)
 
 
+print(y_pred)
 
-print(y_pred.reshape(len(y_pred), 1))
-print(y.reshape(len(y), 1))
+# ---------------------------- Exporting data --------------------
+# dataset_test_reorder["Survived"] = y_pred
+# 
+# csv = dataset_test_reorder[['PassengerId',"Survived"]]
+# 
+# csv.to_csv("submission.csv", index=False)
+
+
+
 
 
 
